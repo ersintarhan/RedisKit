7131ea3aff218da6a2471a7fae393c3804b31b2f
Ersin Tarhan <ersintarhan@gmail.com>
2025-08-17 17:09:03 +0300

    Remove RedisKit test files and refactor PatternMatcher
    Deleted RedisKit.Tests/RedisCacheServiceTests.cs, RedisKit.Tests/RedisDistributedLockTests.cs, and RedisKit.Tests/RedisPubSubServiceTests.cs. Refactored PatternMatcher.cs to use a PatternState object for state management instead of individual variables.

--- RedisKit/Utilities/PatternMatcher.cs
+++ RedisKit/Utilities/PatternMatcher.cs
@@ -38,112 +38,106 @@ namespace RedisKit.Utilities
             var regex = "^";
             var i = 0;
             var len = globPattern.Length;
-            var inClass = false;
-            var inEscape = false;
+            var state = new PatternState();
 
             while (i < len)
             {
                 var c = globPattern[i];
 
-                if (inEscape)
+                if (state.InEscape)
                 {
                     regex += Regex.Escape(c.ToString());
-                    inEscape = false;
+                    state.InEscape = false;
                     i++;
                     continue;
                 }
 
-                switch (c)
-                {
-                    case '\\':
-                        inEscape = true;
-                        i++;
-                        break;
-
-                    case '*':
-                        if (!inClass)
-                        {
-                            regex += ".*";
-                        }
-                        else
-                        {
-                            regex += "\\*";
-                        }
-                        i++;
-                        break;
-
-                    case '?':
-                        if (!inClass)
-                        {
-                            regex += ".";
-                        }
-                        else
-                        {
-                            regex += "\\?";
-                        }
-                        i++;
-                        break;
-
-                    case '[':
-                        if (!inClass)
-                        {
-                            inClass = true;
-                            regex += "[";
-
-                            // Check for negation
-                            if (i + 1 < len && (globPattern[i + 1] == '!' || globPattern[i + 1] == '^'))
-                            {
-                                regex += "^";
-                                i += 2;
-                            }
-                            else
-                            {
-                                i++;
-                            }
-                        }
-                        else
-                        {
-                            regex += "\\[";
-                            i++;
-                        }
-                        break;
-
-                    case ']':
-                        if (inClass)
-                        {
-                            inClass = false;
-                            regex += "]";
-                        }
-                        else
-                        {
-                            regex += "\\]";
-                        }
-                        i++;
-                        break;
-
-                    case '-':
-                        // Inside a character class, dash is range operator
-                        if (inClass && i > 0 && i < len - 1 &&
-                            globPattern[i - 1] != '[' && globPattern[i + 1] != ']')
-                        {
-                            regex += "-";
-                        }
-                        else
-                        {
-                            regex += "\\-";
-                        }
-                        i++;
-                        break;
-
-                    default:
-                        regex += Regex.Escape(c.ToString());
-                        i++;
-                        break;
-                }
+                var (appendText, increment) = ProcessCharacter(c, globPattern, i, len, state);
+                regex += appendText;
+                i += increment;
             }
 
             regex += "$";
             return regex;
+        }
+
+        private static (string appendText, int increment) ProcessCharacter(
+            char c, string pattern, int position, int length, PatternState state)
+        {
+            return c switch
+            {
+                '\\' => ProcessEscape(state),
+                '*' => ProcessAsterisk(state),
+                '?' => ProcessQuestionMark(state),
+                '[' => ProcessOpenBracket(pattern, position, length, state),
+                ']' => ProcessCloseBracket(state),
+                '-' => ProcessDash(pattern, position, length, state),
+                _ => (Regex.Escape(c.ToString()), 1)
+            };
+        }
+
+        private static (string, int) ProcessEscape(PatternState state)
+        {
+            state.InEscape = true;
+            return ("", 1);
+        }
+
+        private static (string, int) ProcessAsterisk(PatternState state)
+        {
+            return state.InClass ? ("\\*", 1) : (".*", 1);
+        }
+
+        private static (string, int) ProcessQuestionMark(PatternState state)
+        {
+            return state.InClass ? ("\\?", 1) : (".", 1);
+        }
+
+        private static (string, int) ProcessOpenBracket(string pattern, int position, int length, PatternState state)
+        {
+            if (state.InClass)
+            {
+                return ("\\[", 1);
+            }
+
+            state.InClass = true;
+            var result = "[";
+            var increment = 1;
+
+            // Check for negation
+            if (position + 1 < length && (pattern[position + 1] == '!' || pattern[position + 1] == '^'))
+            {
+                result += "^";
+                increment = 2;
+            }
+
+            return (result, increment);
+        }
+
+        private static (string, int) ProcessCloseBracket(PatternState state)
+        {
+            if (state.InClass)
+            {
+                state.InClass = false;
+                return ("]", 1);
+            }
+            return ("\\]", 1);
+        }
+
+        private static (string, int) ProcessDash(string pattern, int position, int length, PatternState state)
+        {
+            // Inside a character class, dash is range operator
+            if (state.InClass && position > 0 && position < length - 1 &&
+                pattern[position - 1] != '[' && pattern[position + 1] != ']')
+            {
+                return ("-", 1);
+            }
+            return ("\\-", 1);
+        }
+
+        private sealed class PatternState
+        {
+            public bool InClass { get; set; }
+            public bool InEscape { get; set; }
         }
 
         /// <summary>
